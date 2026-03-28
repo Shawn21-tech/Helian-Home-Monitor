00a2a4a4c1c2796fa85bcd5606b3b5458ef20e7a
Mbuyelo Mabasa <mabasam908@gmail.com>
2026-03-28 22:09:27 +0200

    Add DHT11 sensor reading functionality

---
+++ code/dht11_read.ino
@@ -0,0 +1,34 @@
+# include <DHT.h>
+# define DHTPIN 18
+# define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop()
+{
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("% ");
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" degrees celsius");
+
+  delay(2000);
+}
